b8e7de7cc0dbef98faa73180e98e40e37094ccd7
grain <youmu2012@gmail.com>
2021-12-03 18:21:39 +0800

    replace cdnjs.cloudflare.com

--- src/settings.js
+++ src/settings.js
@@ -1,93 +1,9 @@
 (function () {
-    // chrome.storage.local.get("pattern", function (result) {
-    //     var txt = document.getElementById("requst-patterns");
-    //     console.log(JSON.parse(result.pattern));
-    //     txt.value = result.pattern;
-    // });
-
-    // document.getElementById("save").onclick = function () {
-    //     var txt = document.getElementById("requst-patterns");
-    //     console.log(txt.value);
-    //     var items = {"pattern": txt.value};
-    //     console.log(items);
-    //     chrome.storage.local.set(items, function(){
-    //         console.log("saved"); 
-    //     });
-    // }
-
     var defaultPatterns = [
         "*://www.google-analytics.com/*",
         "*://google-analytics.com/*",
         "*://ssl.google-analytics.com/*",
-        "*://*.g.doubleclick.net/*",
-
-        "*://engine.adzerk.net/*",
-
-        "*://hm.baidu.com/*",
-        "*://eclick.baidu.com/*",
-        "*://pos.baidu.com/*",
-        "*://drmcmm.baidu.com/*",
-        "*://cpro.baidu.com/*",
-        "*://datax.baidu.com/*",
-        "*://nsclick.baidu.com/*",
-        "*://cpro.baidustatic.com/*",
-        "*://dup.baidustatic.com/*",
-
-        "*://pss.alicdn.com/*",
-        "*://afptrack.alimama.com/*",
-        "*://afpeng.alimama.com/*",
-        "*://dsp.simba.taobao.com/*",
-        "*://m.simba.taobao.com/*",
-        "*://textlink.simba.taobao.com/*",
-        "*://show.re.taobao.com/*",
-
-        "*://sax.sina.com.cn/*",
-        "*://beacon.sina.com.cn/*",
-
-        "*://btrace.qq.com/*",
-        "*://btrace.video.qq.com/*",
-
-        "*://api.growingio.com/*",
-        "*://g.cn.miaozhen.com/*",
-        "*://t.cr-nielsen.com/*",
-        "*://yt.mmstat.com/*",
-
-        "*://gm.mmstat.com/*",
-        "*://atm.youku.com /*",
-        "*://fvid.atm.youku.com/*",
-        "*://html.atm.youku.com/*",
-        "*://valb.atm.youku.com/*",
-        "*://valf.atm.youku.com/*",
-        "*://valo.atm.youku.com/*",
-        "*://valp.atm.youku.com/*",
-        "*://Lstat.youku.com/*",
-        "*://speed.lstat.youku.com/*",
-        "*://urchin.lstat.youku.com/*",
-        "*://stat.youku.com/*",
-        "*://static.lstat.youku.com/*",
-        "*://valc.atm.youku.com/*",
-        "*://vid.atm.youku.com/*",
-        "*://walp.atm.youku.com/*",
-        "*://count.atm.youku.com/*",
-
-        "*://cl.webterren.com/*",
-        "*://sb.scorecardresearch.com/*",
-        "*://mon.xtgreat.com/*",
-        "*://m.reachmax.cn/*",
-
-        "*://cm.cn.miaozhen.com/*",
-        "*://cm.masky.biddingx.com/*",
-        "*://ana.masky.biddingx.com/*",
-        "*://shunfei-cm.cn.miaozhen.com/*",
-        "*://cms.tanx.com/*",
-        "*://ana.stg8.com/*",
-        "*://pcookie.tanx.com/*",
-
-        "*://163.wrating.com/*",
-        "*://webzj.reg.163.com/*",
-        "*://web.stat.ws.126.net/*",
-
-        "*://ads.csdn.net/*"
+        "*://*.g.doubleclick.net/*"
     ];
 
     var patterns = localStorage['pattern'];
--- src/stop.js
+++ src/stop.js
@@ -1,8 +1,3 @@
-//var patterns = [];
-// chrome.storage.local.get("pattern", function (result) {
-//     patterns = JSON.parse(result.pattern);
-// });
-
 var p = localStorage['pattern'];
 var patterns = p !== undefined ? JSON.parse(p) : [];
 
@@ -20,30 +15,22 @@ if (patterns !== undefined && patterns.length > 0) {
     );
 }
 
-
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
+        if (details.url.indexOf('cdnjs.cloudflare.com/ajax/libs/') > -1) {
+            return { redirectUrl: details.url.replace('cdnjs.cloudflare.com/ajax/libs/', 'cdn.staticfile.org/') }
+        } else if (details.url.indexOf('www.google.com/recaptcha/') > -1) {
+            return { redirectUrl: details.url.replace("www.google.com", "www.recaptcha.net") };
+        }
         var m = details.url.match(/([0-9][0-9\.]*[0-9])/g);
         return { redirectUrl: "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-"+m[0]+".min.js" };
     },
     {
         urls: [
             "*://ajax.googleapis.com/ajax/libs/jquery/*",
-            "*://code.jquery.com/jquery*.js"
-        ]
-    },
-    [
-        "blocking"
-    ]
-);
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        return { redirectUrl: details.url.replace("www.google.com", "www.recaptcha.net") };
-    },
-    {
-        urls: [
-            "*://www.google.com/recaptcha/*"
+            "*://code.jquery.com/jquery*.js",
+            "*://www.google.com/recaptcha/*",
+            "*://cdnjs.cloudflare.com/ajax/libs/*"
         ]
     },
     [
